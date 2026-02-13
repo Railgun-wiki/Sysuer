2a708559ee1c4e1f47fcd3c3a07c12894c6c59ce
SYSU-Tang <2658481919@qq.com>
2026-02-13 10:25:40 +0800

    优化校车界面

--- app/src/main/java/com/sysu/edu/life/SchoolBusActivity.java
+++ app/src/main/java/com/sysu/edu/life/SchoolBusActivity.java
@@ -1,5 +1,7 @@
 package com.sysu.edu.life;
 
+import static com.sysu.edu.api.CommonUtil.extractValue;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -7,6 +9,7 @@ import android.os.Message;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -22,32 +25,35 @@ import com.sysu.edu.view.StaggeredFragment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class SchoolBusActivity extends AppCompatActivity {
     HttpManager http;
+    JSONObject data;
+    MutableLiveData<Boolean> day = new MutableLiveData<>(true);
+    ArrayList<String> routes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityPagerBinding binding = ActivityPagerBinding.inflate(getLayoutInflater());
-        final ArrayList<String> routes = new ArrayList<>();
         setContentView(binding.getRoot());
         binding.toolbar.setNavigationOnClickListener(_ -> supportFinishAfterTransition());
         Params params = new Params(this);
         params.setCallback(this::getData);
         binding.toolbar.setTitle(R.string.school_bus);
-        Pager2Adapter adp = new Pager2Adapter(this);
-        binding.pager.setAdapter(adp);
+        Pager2Adapter pager2Adapter = new Pager2Adapter(this);
+        binding.pager.setAdapter(pager2Adapter);
         ItemSchoolBusNoticeBinding header = ItemSchoolBusNoticeBinding.inflate(getLayoutInflater(), binding.appBarLayout, false);
-        header.day.setOnClickListener(_ -> {
-            boolean selected = header.day.isSelected();
-            header.day.setText(selected ? R.string.workday : R.string.weekend);
-            header.day.setSelected(!selected);
-        });
+        header.day.setOnClickListener(_ -> day.setValue(Boolean.FALSE.equals(day.getValue())));
         AlertDialog notice = new MaterialAlertDialogBuilder(this).setTitle(R.string.notice).create();
         header.notice.setOnClickListener(_ -> notice.show());
-        header.option.setOnItemClickListener((_, _, position, _) -> {
-            binding.pager.setCurrentItem(position);
+        header.option.setOnItemClickListener((_, _, position, _) -> binding.pager.setCurrentItem(position));
+        day.observe(this, b -> {
+            b = Boolean.TRUE.equals(b);
+            loadRoute(b ? "workDay" : "holiday", header, notice, pager2Adapter);
+            header.day.setText(b ? R.string.workday : R.string.holiday);
         });
         binding.appBarLayout.addView(header.getRoot());
         new TabLayoutMediator(binding.tabs, binding.pager, (tab, position) -> tab.setText(routes.get(position))).attach();
@@ -63,26 +69,8 @@ public class SchoolBusActivity extends AppCompatActivity {
                     JSONObject response = JSONObject.parseObject((String) msg.obj);
                     if (response != null && response.getJSONObject("meta").getInteger("statusCode").equals(200) && response.get("data") != null) {
                         if (msg.what == 0) {
-                            /*holiday*/
-                            response.getJSONObject("data").getJSONArray("workDay").forEach(a -> {
-                                StaggeredFragment fragment = new StaggeredFragment();
-                                JSONObject item = (JSONObject) a;
-                                routes.add(item.getString("drivingDirectionName"));
-                                ArrayList<String> infos = new ArrayList<>();
-                                for (String i : new String[]{"drivingDirectionName", "startStation", "endStation"}) {
-                                    infos.add(item.getString(i));
-                                }
-                                header.option.setSimpleItems(routes.toArray(new String[0]));
-                                notice.setMessage(item.getString("note"));
-                                fragment.add(SchoolBusActivity.this, getString(R.string.route_detail), List.of("路线", "起点", "终点"), infos);
-                                adp.add(fragment);
-                                item.getJSONArray("schoolBusShuttleMomentList").forEach(b -> {
-                                    ArrayList<String> values = new ArrayList<>();
-                                    for (String i : new String[]{"passenger", "vehiclesType", "time", "drivingRoute"})
-                                        values.add(((JSONObject) b).getString(i));
-                                    fragment.add(SchoolBusActivity.this, values.get(2),  List.of("乘客", "车辆", "时间", "路线"), values);
-                                });
-                            });
+                            data = response.getJSONObject("data");
+                            day.setValue(Boolean.TRUE);
                         }
                     } else {
                         params.toast(getString(R.string.login_warning));
@@ -93,6 +81,35 @@ public class SchoolBusActivity extends AppCompatActivity {
         });
         http.setParams(params);
         getData();
+    }
+
+    void loadRoute(String day, ItemSchoolBusNoticeBinding header, AlertDialog notice, Pager2Adapter adp) {
+        AtomicInteger i = new AtomicInteger(0);
+        if (data != null) {
+            if (data.getJSONArray(day).isEmpty()) {
+                IntStream.range(0, adp.getItemCount()).forEach(j -> ((StaggeredFragment) adp.getItem(j)).clear());
+            } else {
+                data.getJSONArray(day).forEach(a -> {
+                    JSONObject item = (JSONObject) a;
+                    StaggeredFragment fragment;
+                    if (adp.getItemCount() > i.get()) {
+                        fragment = (StaggeredFragment) adp.getItem(i.get());
+                        fragment.clear();
+                    } else {
+                        routes.add(item.getString("drivingDirectionName"));
+                        fragment = new StaggeredFragment();
+                        adp.add(fragment);
+                    }
+                    i.getAndIncrement();
+                    notice.setMessage(item.getString("note"));
+                    fragment.add(SchoolBusActivity.this, getString(R.string.route_detail), List.of("路线", "起点", "终点"),
+                            extractValue(item, new String[]{"drivingDirectionName", "startStation", "endStation"}));
+                    item.getJSONArray("schoolBusShuttleMomentList").forEach(b -> fragment.add(SchoolBusActivity.this, ((JSONObject) b).getString("time"), List.of("乘客", "车辆", "时间", "路线"),
+                            extractValue(((JSONObject) b), new String[]{"passenger", "vehiclesType", "time", "drivingRoute"})));
+                });
+                header.option.setSimpleItems(routes.toArray(new String[0]));
+            }
+        }
     }
 
     void getData() {
