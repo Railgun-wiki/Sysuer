ad851545f53d65fea3f76adabefe4bfa43a8db09
SYSU-Tang <2658481919@qq.com>
2026-04-20 22:29:25 +0800

    fix:优化验证码加载方式

--- app/src/main/java/com/sysu/edu/life/ComplaintMainFragment.java
+++ app/src/main/java/com/sysu/edu/life/ComplaintMainFragment.java
@@ -14,9 +14,6 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.sysu.edu.databinding.FragmentComplaintMainBinding;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 public class ComplaintMainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,18 +23,8 @@ public class ComplaintMainFragment extends Fragment {
         return binding.getRoot();
     }
 
-    private void loadCaptcha(ShapeableImageView captcha) {
-
-        try {
-            captcha.setImageBitmap(CompletableFuture.supplyAsync(() -> {
-                try {
-                return Glide.with(requireContext()).asBitmap().load(Uri.parse("https://xinfang.sysu.edu.cn/servlet/checkcode")).diskCacheStrategy(DiskCacheStrategy.NONE).override(300, 120).submit().get();
-                } catch (ExecutionException | InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }).get());
-        } catch (ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    void loadCaptcha(ShapeableImageView imageView) {
+        Glide.with(requireContext()).load(Uri.parse("https://xinfang.sysu.edu.cn/servlet/checkcode")).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).override(300, 120).into(imageView);
     }
+
 }
